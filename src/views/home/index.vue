9972b1c447af537340203a2d594a72e4d66145a7
frustrated489 <489394204@qq.com>
2019-12-01 20:20:19 +0800

    首页-展示频道列表

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -14,11 +14,6 @@
           </van-list>
         </van-pull-refresh>
       </van-tab>
-      <van-tab title="标签 2">内容 2</van-tab>
-      <van-tab title="标签 3">内容 3</van-tab>
-      <van-tab title="标签 4">内容 4</van-tab>
-      <van-tab title="标签 5">内容 5</van-tab>
-      <van-tab title="标签 6">内容 6</van-tab>
     </van-tabs>
     <!-- 频道列表 -->
   </div>
